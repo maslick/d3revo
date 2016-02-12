4dfd5570ebccf92daa9e40c7e1db45ee9881c0cc
Pavel Maslov <pavel.maslov@cloud.si>
2016-02-12 18:12:09 +0100

    added margins, small fixes

--- js/d3revo.js
+++ js/d3revo.js
@@ -7,16 +7,23 @@ angular.module('d3revo', ['angular-ladda'])
     ])
     .controller('d3revoCtrl', ['$scope', '$http', function ($scope, $http) {
 
-        var width = $("#tree").width();
-        var height = $("#tree").height();
+        var margin = {top: -5, right: -5, bottom: -5, left: 250},
+            width = 800 - margin.left - margin.right,
+            height = 600 - margin.top - margin.bottom;
+
+        var tree = d3.layout.tree()
+            .size([height,width]);
+
+        var diagonal = d3.svg.diagonal()
+            .projection(function(d) {return [d.y, d.x]});
 
 
         var canvas = d3.select("#tree").append("svg")
             .attr("width", "100%")
-            .attr("height", "100%")
+            .attr("height", height + margin.top + margin.bottom)
             .append("g")
-            .attr("transform", "translate(0,0)")
-            .call(d3.behavior.zoom().scaleExtent([0.3, 1]).on("zoom", function(){
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .call(d3.behavior.zoom().scaleExtent([0.3, 10]).on("zoom", function(){
                 canvas.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
             }))
             .append("g");
@@ -28,15 +35,18 @@ angular.module('d3revo', ['angular-ladda'])
             .attr("width", width)
             .attr("height", height);
 
-        var tree = d3.layout.tree()
-            .size([800,600]);
 
         d3.json("js/data.json", function(data) {
+            data.x0 = height / 2;
+            data.y0 = 0;
 
-            var nodes = tree.nodes(data);
+            var nodes = tree.nodes(data).reverse();
             var links = tree.links(nodes);
 
-            var node = canvas.selectAll(".node")
+            // Normalize for fixed-depth
+            nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+            var node = canvas.selectAll("g.node")
                 .data(nodes)
                 .enter()
                 .append("g")
@@ -47,14 +57,16 @@ angular.module('d3revo', ['angular-ladda'])
 
             node.append("circle")
                 .attr("r",5)
-                .attr("fill", "steelblue");
+                .attr("fill", "lightsteelblue");
 
             node.append("text")
-                .text(function(d) {return d.name;});
+                .text(function(d) {return d.name;})
+                .attr("x", function(d){return -10;})
+                .attr("y", function(d) {return 0;})
+                .attr("dy", ".35em")
+                .attr("text-anchor", function(d) { return "end"; })
 
 
-            var diagonal = d3.svg.diagonal()
-                .projection(function (d) {return [d.y, d.x]})
 
             canvas.selectAll(".link")
                 .data(links)
